97bb69ea7c8a8c452fdb6785acdad5c734dccb8f
txssu <z.ribin20@gmail.com>
2025-06-13 17:31:01 +0500

    Add space between inputs

--- lib/any_talker_web/lives/webapp/global_config_live.ex
+++ lib/any_talker_web/lives/webapp/global_config_live.ex
@@ -16,16 +16,16 @@ defmodule AnyTalkerWeb.WebApp.GlobalConfigLive do
 
     <.section class="mt-5">
       <:header>Настройки</:header>
-      <div class="space-y-2 px-2">
-        <.form for={@form} phx-change="save">
+      <.form for={@form} phx-change="save">
+        <div class="space-y-3">
           <.tg_input label="Модель /ask" field={@form[:ask_model]} />
           <.tg_input type="number" label="Лимит запросов /ask" field={@form[:ask_rate_limit]} />
           <.tg_input type="number" label="Период лимита /ask (мс)" field={@form[:ask_rate_limit_scale_ms]} />
           <div class="mt-2">
             <.textarea label="Промпт /ask" field={@form[:ask_prompt]} />
           </div>
-        </.form>
-      </div>
+        </div>
+      </.form>
     </.section>
     """
   end
