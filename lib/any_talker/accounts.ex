23bd0fea67cb2451e6e1e0916358ea4c7c4ef630
txssu <z.ribin20@gmail.com>
2025-06-14 15:41:41 +0500

    Reorder functions

--- lib/any_talker/accounts.ex
+++ lib/any_talker/accounts.ex
@@ -8,6 +8,11 @@ defmodule AnyTalker.Accounts do
   alias AnyTalker.Repo
   alias AnyTalker.Settings.ChatConfig
 
+  @spec get_user(integer()) :: User.t() | nil
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
   @spec upsert_user(map(), [atom()]) :: {:ok, User.t()} | {:error, Ecto.Changeset.t()}
   def upsert_user(attrs, keys) do
     %User{}
@@ -20,6 +25,11 @@ defmodule AnyTalker.Accounts do
     user
     |> User.changeset(attrs)
     |> Repo.update()
+  end
+
+  @spec change_user(User.t(), map()) :: Ecto.Changeset.t()
+  def change_user(user, attrs \\ %{}) do
+    User.changeset(user, attrs)
   end
 
   @spec create_token(User.t()) :: String.t()
@@ -64,16 +74,6 @@ defmodule AnyTalker.Accounts do
   @spec owner?(User.t()) :: boolean()
   def owner?(%User{id: user_id}) do
     Application.get_env(:any_talker, :owner_id) == user_id
-  end
-
-  @spec get_user(integer()) :: User.t() | nil
-  def get_user(id) do
-    Repo.get(User, id)
-  end
-
-  @spec change_user(User.t(), map()) :: Ecto.Changeset.t()
-  def change_user(user, attrs \\ %{}) do
-    User.changeset(user, attrs)
   end
 
   @spec display_name(User.t() | nil) :: String.t() | nil
